026c08c7eb094c350f82d582818e34304b9c4ad8
shanilboodhooa <121301878+shanilboodhooa@users.noreply.github.com>
2023-11-26 21:05:15 +0400

    Delete XO Game directory

--- XO Game/scripe.js
+++
@@ -1,9 +0,0 @@
-let comp = document.getElementById("comp");
-let passplay = document.getElementById("passplay")
-comp.addEventListener("click", ()=>{
-    location.href = "botUI.html"
-})
-
-passplay.addEventListener("click", ()=>{
-    location.href = "Local.html"
-})
